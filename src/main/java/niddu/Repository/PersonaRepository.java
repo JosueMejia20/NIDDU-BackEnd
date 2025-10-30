d9546490d56220b846198762788982f4be06759b
Roberto Matamoros <65750735+Hunter774@users.noreply.github.com>
2025-10-29 19:09:31 -0600

    asdasd

--- src/main/java/niddu/Repository/PersonaRepository.java
+++ src/main/java/niddu/Repository/PersonaRepository.java
@@ -1,9 +1,7 @@
-package niddu.Repository;
+package niddu.repository;
 
-import niddu.Model.Persona;
+import niddu.model.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PersonaRepository extends JpaRepository<Persona, Integer> {
 }
