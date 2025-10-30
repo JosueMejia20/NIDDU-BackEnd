67931d81a582dbb6a355e09a28a12380fe8e5360
Roberto Matamoros <65750735+Hunter774@users.noreply.github.com>
2025-10-29 18:00:26 -0600

    Arreglado clase personas

--- src/main/java/niddu/Model/personas.java
+++ src/main/java/niddu/Model/personas.java
@@ -1,25 +1,24 @@
-package com.niddu.model;
+package com.Niddu.model;
 
 import jakarta.persistence.*;
 
 @Entity
-public class personas {
+public class Persona {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id_perona;
+    private Long id_persona;
 
     private String nombres;
     private String email;
 
     // Getters y setters
-    public Long getId() { return id_perona; }
-    public void setId(Long id_perona) { this.id_perona = id_perona; }
+    public Long getId() { return id_persona; }
+    public void setId(Long id_persona) { this.id_persona = id_persona; }
 
     public String getNombres() { return nombres; }
-    public void setNombre(String nombres) { this.nombres = nombres; }
+    public void setNombres(String nombres) { this.nombres = nombres; }
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 }
-
