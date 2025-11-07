0dbb94f45205ff57f7dd18718eb860103fbdb061
Roberto <Roberto@ROBBER>
2025-11-06 23:01:48 -0600

    Campo Foto_identidad agregado a Persona

--- src/main/java/niddu/Models/Persona.java
+++ src/main/java/niddu/Models/Persona.java
@@ -24,6 +24,9 @@ public class Persona {
     @Column(name = "telefono", length = 20)
     private String telefono;
 
+    @Column(name = "foto_identidad", length = 255)
+    private String fotoIdentidad;
+
     @OneToOne(mappedBy = "persona")
     @JsonIgnore
     private Usuario usuario;
