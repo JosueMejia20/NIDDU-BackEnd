bbef8741f77f15afc33867245910ee987ea2c474
Mauricio Alberto Velásquez Díaz <vemauricio20@gmail.com>
2025-11-24 02:37:17 -0600

    cambiando la version de java por el error en el deploy

--- src/main/java/niddu/controllers/ServicioController.java
+++ src/main/java/niddu/controllers/ServicioController.java
@@ -4,12 +4,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import niddu.Models.Servicio;
 import niddu.Models.Dtos.ServicioCompletoDto;
 import niddu.Services.ServicioService;
-import org.springframework.web.bind.annotation.GetMapping;
 
-
+@CrossOrigin(origins = "https://proud-beach-0a296230f.3.azurestaticapps.net:3000")
 @RestController
 @RequestMapping("/servicios")
 public class ServicioController {
